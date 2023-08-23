0248e33fadabffb6f24f3d9624ea4f6f03c076b4
jaymesC <clementjaymes@gmail.com>
2023-08-23 06:34:38 -0700

    fix: event text positioning

--- src/components/organisms/event-section/index.tsx
+++ src/components/organisms/event-section/index.tsx
@@ -51,7 +51,7 @@ export default function EventSection() {
           </div>
         </div>
       </div>
-      <h2 className="text-center md:text-2xl md:mt-16 mt-10 italic text-lg">
+      <h2 className="text-center md:text-2xl md:mt-16 my-10 italic text-lg">
         Come and experience a total turn around with the King of Kings...{' '}
       </h2>
     </div>
