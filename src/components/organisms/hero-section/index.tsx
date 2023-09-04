400a9c9067ec825deaf77988c40e06b12c57f588
Rex Raphael <rex.raphael@outlook.com>
2023-09-04 12:33:51 -0500

    chore: imported content data

--- src/components/organisms/hero-section/index.tsx
+++ src/components/organisms/hero-section/index.tsx
@@ -27,7 +27,8 @@ export default function HeroSection() {
         <h2 className="text-md font-semibold">DEC 12th - 15th</h2>
       </div>
       <h1 className="font-['Inter'] mt-32 text-black text-5xl md:text-7xl lg:text-9xl font-black max-w-7xl text-center tracking-widest main-header-text">
-        THE CROSS <span className="bg-black text-white">CONFERENCE</span> 2023
+        THE CROSS <span className="bg-black text-white px-4">CONFERENCE</span>{' '}
+        2023
       </h1>
       <div className="flex items-center justify-center max-w-2xl">
         <span>
