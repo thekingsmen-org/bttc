72bc019e920d865020419609af12135bd4ae6164
Rex Raphael <rex.raphael@outlook.com>
2023-09-04 18:13:58 -0500

    chore: imported content data

--- src/clients/appwrite.ts
+++ src/clients/appwrite.ts
@@ -1,18 +1,10 @@
-import { Client, Databases, Storage } from 'appwrite'
+import { Client, Databases } from 'appwrite'
 
 const client = new Client()
-const client2 = new Client()
-
-client2
-  .setEndpoint(`${process.env.NEXT_PUBLIC_APP_ENDPOINT}`)
-  .setProject(`${process.env.NEXT_PUBLIC_PROJECT_ID}`)
-
 client
   .setEndpoint(`${process.env.NEXT_PUBLIC_ENDPOINT}`)
   .setProject(`${process.env.NEXT_PUBLIC_PROJECT_ID}`)
 
 export const databases = new Databases(client)
-export const storage = new Storage(client2)
-export const getBucketID = () => process.env.NEXT_PUBLIC_BUCKET_ID ?? ''
 
 export default client
